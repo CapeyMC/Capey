6fc74ace075b5e6cf3912daf8d1a9bc0fe6ef42d
jgj52 <email@jgj52.hu>
2026-05-16 15:42:28 +0200

    load textures

--- src/main/java/hu/jgj52/capey/types/Cape.java
+++ src/main/java/hu/jgj52/capey/types/Cape.java
@@ -1,12 +1,23 @@
 package hu.jgj52.capey.types;
 
+import com.mojang.blaze3d.platform.NativeImage;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.resources.Identifier;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.resources.
+//? >= 1.21.11 {
+Identifier
+//? } else {
+/*ResourceLocation
+*///? }
+;
 
+import java.io.File;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,32 +28,65 @@ import java.util.concurrent.Executors;
 public class Cape {
     private static final ExecutorService fetcher = Executors.newVirtualThreadPerTaskExecutor();
     private static final HttpClient client = HttpClient.newHttpClient();
+    private static final Minecraft mc = Minecraft.getInstance();
     private static final Map<UUID, Cape> capes = new HashMap<>();
     public static Cape of(UUID uuid) {
         return capes.computeIfAbsent(uuid, Cape::new);
     }
 
-    private volatile Identifier identifier;
+    private final UUID uuid;
+    private final Path path;
+    private volatile
+    //? >= 1.21.11 {
+    Identifier
+    //? } else {
+    /*ResourceLocation
+     *///? }
+    identifier;
 
     private Cape(UUID uuid) {
-        fetcher.submit(() -> {
-            try {
-                HttpRequest request = HttpRequest.newBuilder()
-                        .uri(new URI("https://capey.jgj52.hu/cape/" + uuid))
-                        .GET()
-                        .build();
+        this.uuid = uuid;
+        path = Path.of(FabricLoader.getInstance().getGameDir().toString(), "capey", uuid.toString());
+        File file = new File(path.toUri());
+        if (file.exists()) {
+            registerTextures();
+        } else {
+            fetcher.submit(() -> {
+                try {
+                    HttpRequest request = HttpRequest.newBuilder()
+                            .uri(new URI("https://capey.jgj52.hu/cape/" + uuid))
+                            .GET()
+                            .build();
 
-                Path path = Path.of(FabricLoader.getInstance().getGameDir().toString(), "capey", uuid.toString());
-                HttpResponse<Path> response = client.send(request, HttpResponse.BodyHandlers.ofFile(path));
-                if (response.statusCode() == 404) {
-                    identifier = null;
+                    HttpResponse<Path> response = client.send(request, HttpResponse.BodyHandlers.ofFile(path));
+                    if (response.statusCode() == 404) {
+                        identifier = null;
+                    }
+                    if (response.statusCode() == 200) {
+                        registerTextures();
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
                 }
-                if (response.statusCode() == 200) {
+            });
+        }
+    }
 
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        });
+    private void registerTextures() {
+        try {
+            NativeImage img = NativeImage.read(Files.newInputStream(path));
+            identifier = //? >= 1.21.11 {
+                    Identifier
+                    //? } else {
+                    /*ResourceLocation
+                     *///? }
+                    .fromNamespaceAndPath("capey", "capes/" + uuid);
+            mc.execute(() -> {
+                DynamicTexture texture = new DynamicTexture(() -> "", img);
+                texture.upload();
+            });
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
