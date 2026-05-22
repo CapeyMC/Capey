e252a34919b7ce713b4094c13189d2a2fca1ceaf
jgj52 <email@jgj52.hu>
2026-05-22 19:49:09 +0200

    switch to trender but ill have to fix a lot of things

--- src/main/java/hu/jgj52/capey/config/ModMenu.java
+++ src/main/java/hu/jgj52/capey/config/ModMenu.java
@@ -3,10 +3,11 @@ package hu.jgj52.capey.config;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import hu.jgj52.capey.screen.ConfigScreen;
+import net.minecraft.network.chat.Component;
 
 public class ModMenu implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return ConfigScreen::new;
+        return parent -> new ConfigScreen(Component.translatable("capey.config.main.title"), parent).createScreen();
     }
 }
--- src/main/java/hu/jgj52/capey/screen/ConfigScreen.java
+++ src/main/java/hu/jgj52/capey/screen/ConfigScreen.java
@@ -1,49 +1,84 @@
 package hu.jgj52.capey.screen;
 
+import com.mojang.authlib.GameProfile;
+import dev.tr7zw.trender.gui.client.AbstractConfigScreen;
+import dev.tr7zw.trender.gui.widget.WGridPanel;
+import dev.tr7zw.trender.gui.widget.data.Insets;
 import hu.jgj52.capey.types.Cape;
 import hu.jgj52.capey.types.Player;
-import hu.jgj52.screenapi.screen.BetterScreen;
+import hu.jgj52.capey.widget.PlayerPreview;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.GuiGraphicsExtractor;
-import net.minecraft.client.gui.components.PlayerSkinWidget;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.RemotePlayer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.PlayerModelPart;
+import net.minecraft.world.entity.player.PlayerSkin;
+import org.jspecify.annotations.NonNull;
 
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
 
-public class ConfigScreen extends BetterScreen {
+public class ConfigScreen extends AbstractConfigScreen {
+    private static class FakePlayer extends RemotePlayer {
+        private final Supplier<PlayerSkin> skin;
+
+        public FakePlayer(ClientLevel level, GameProfile gameProfile, Supplier<PlayerSkin> skin) {
+            super(level, gameProfile);
+            this.skin = skin;
+
+            byte skinParts = 0;
+            for (PlayerModelPart part : PlayerModelPart.values()) {
+                skinParts |= (byte) part.getMask();
+            }
+            getEntityData().set(net.minecraft.world.entity.player.Player.DATA_PLAYER_MODE_CUSTOMISATION, skinParts);
+        }
+
+        @Override
+        public @NonNull PlayerSkin getSkin() {
+            return skin.get();
+        }
+    }
+
     private static final Minecraft mc = Minecraft.getInstance();
-    private final Screen parent;
-    public ConfigScreen(Screen parent) {
-        this.parent = parent;
+
+    public ConfigScreen(Component title, Screen previous) {
+        super(title, previous);
+
+        WGridPanel root = new WGridPanel(0);
+        root.setInsets(Insets.ROOT_PANEL);
+        setRootPanel(root);
+
+        if (mc.level != null) {
+            AtomicInteger offset = new AtomicInteger();
+            Cape.all(false).forEach(capeO -> {
+                Player player = Player.of(UUID.fromString(capeO.get("uploader").getAsString()));
+                Cape cape = Cape.of(UUID.fromString(capeO.get("uuid").getAsString()));
+                GameProfile profile = new GameProfile(player.getUUID(), "player");
+                Supplier<PlayerSkin> skinWithCape = cape.fromSkin(player.getSkin());
+
+                FakePlayer mcPlayer = new FakePlayer(
+                        mc.level,
+                        profile,
+                        skinWithCape
+                );
+                PlayerPreview preview = new PlayerPreview(mcPlayer);
+                preview.setRotationX(164);
+                preview.setRotationY(5);
+                preview.setShowBackground(true);
+                root.add(preview, offset.getAndIncrement() * 10 + 70, 14, 70, 140);
+            });
+        }
     }
 
     @Override
-    protected int getScrollSpeed() {
-        return 10;
+    public void save() {
+
     }
 
     @Override
-    public void render(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float v) {
-        graphics.fill(40, 20, width - 40, height - 20, 0x80000000);
-    }
+    public void reset() {
 
-    @Override
-    protected void createWidgets(Font font) {
-        Cape.all(false).forEach(capeO -> {
-            Player player = Player.of(UUID.fromString(capeO.get("uploader").getAsString()));
-            Cape cape = Cape.of(UUID.fromString(capeO.get("uuid").getAsString()));
-            widget(new PlayerSkinWidget(
-                    72,
-                    144,
-                    mc.getEntityModels(),
-                    cape.fromSkin(player.getSkin())
-            ));
-        });
-    }
-
-    @Override
-    public void onClose() {
-        mc.setScreen(parent);
     }
 }
